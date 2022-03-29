862b9dbea40e88016f23d0312b7673233b8bf1af
j-cort <81960011+j-cort@users.noreply.github.com>
2022-03-29 09:43:48 +0100

    set up mongoose

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
-const parse = require('csv-parse');
 
-fs.readFile('jokes.csv', (err, data) => {
-  parse(data, {}, (err, jokes) => {
-    app.get('/', function (req, res) {
-      let i = Math.floor((Math.random() * jokes.length));
-      let response = {
-        joke: jokes[i][0]
-      }
-      res.json(response);
-    });
-  });
+// MODELS
+
+// MONGOOSE
+mongoose
+  .connect('mongodb://127.0.0.1:27017/jokes_app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Mongo Connection Open!"))
+  .catch((err) => console.log("Mongo Connection Error!!", err));
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+  // if(req.session.viewCount) {
+  //   req.session.viewCount++
+  // } else {
+  //   req.session.viewCount = 1
+  // }
+  res.send("home");
 });
 
-app.listen(3050, function () {
-  console.log('Example app listening on port 3050.');
+app.listen(3050, () => {
+  console.log("Listening on port 3050.");
 });
+
+
+
+// const fs = require("fs");
+// fs.readFile("jokes.csv", (err, data) => {
+//   parse(data, {}, (err, jokes) => {
+//     app.get("/", function (req, res) {
+//       let i = Math.floor(Math.random() * jokes.length);
+//       let response = {
+//         joke: jokes[i][0],
+//       };
+//       res.json(response);
+//     });
+//   });
+// });
+
+// const parse = require("csv-parse");
